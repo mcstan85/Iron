8eb985d5dff39694b6d3c31a8a1137b1bc2c71a5
Levi <115207729+mcstan85@users.noreply.github.com>
2023-10-05 19:10:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -34,14 +34,14 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001850808865"))
 
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","мαяк ✗")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "1846947483").split())
+    map(int, getenv("OWNER_ID", "6068715364").split())
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
