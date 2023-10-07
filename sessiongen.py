f714f8bda12e1bef889f3f0f61c81626cb996587
Levi <115207729+mcstan85@users.noreply.github.com>
2023-10-07 18:51:44 +0530

    Update sessiongen.py

--- sessiongen.py
+++ sessiongen.py
@@ -8,14 +8,14 @@ API_HASH = input("\nEnter Your API HASH :\n > ")
 
 print("\n\nEnter the phone number associated with your telegram account when asked.\n\n")
 
-fallen = Client("MarkBot", api_id=API_ID, api_hash=API_HASH, in_memory=True)
+fallen = Client("MarkMusic", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 
 
 async def main():
-    await MarkBot.start()
-    sess = await MarkBot.export_session_string()
+    await MarkMusic.start()
+    sess = await MarkMusic.export_session_string()
     txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @marrkmusic"
-    ok = await MarkBot.send_message("me", txt)
+    ok = await MarkMusic.send_message("me", txt)
     print(f"Here is your Pyrogram {ver} String Session\n{sess}\nDouble click to copy.") 
 
 
